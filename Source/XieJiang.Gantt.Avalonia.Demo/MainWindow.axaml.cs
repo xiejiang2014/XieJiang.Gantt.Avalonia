31ca6c8304ec3e2d193a78154ea9ed606b2c5b30
xiejiang <xiejiang2014@icloud.com>
2025-01-05 03:23:10 +0800

    +DemoProj

---
+++ Source/XieJiang.Gantt.Avalonia.Demo/MainWindow.axaml.cs
@@ -0,0 +1,86 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System;
+
+namespace XieJiang.Gantt.Avalonia.Demo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var ganttModel = new GanttModel();
+            ganttModel.GanttTasks.Add(new GanttTask()
+            {
+                Id = 1,
+                Progress = 0.5d,
+                StartDate = new DateTime(2024, 12, 21),
+                EndDate = new DateTime(2024, 12, 23),
+            });
+
+            ganttModel.GanttTasks.Add(new GanttTask()
+            {
+                Id = 2,
+                Progress = 0.2d,
+                StartDate = new DateTime(2024, 12, 23),
+                EndDate = new DateTime(2024, 12, 26),
+            });
+
+            ganttModel.GanttTasks.Add(new GanttTask()
+            {
+                Id = 3,
+                Progress = 0.7d,
+                StartDate = new DateTime(2024, 12, 27),
+                EndDate = new DateTime(2024, 12, 31),
+            });
+
+            ganttModel.GanttTasks[0].AddingDependentTask(ganttModel.GanttTasks[1]);
+            //ganttModel.GanttTasks[1].AddingDependentTask(ganttModel.GanttTasks[2]);
+
+            GanttControl.DataContext = ganttModel;
+        }
+
+        protected override void OnLoaded(RoutedEventArgs e)
+        {
+            base.OnLoaded(e);
+
+            GanttControl.Reload();
+        }
+
+
+        private void GanttControl_OnDependencyLinePointerPressed(object? sender, DependencyLinePointerPressedEventArgs e)
+        {
+        }
+
+        private void ButtonReload_OnClick(object? sender, RoutedEventArgs e)
+        {
+            GanttControl.Reload();
+        }
+
+        private void ButtonToday_OnClick(object? sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonWeekly_OnClick(object? sender, RoutedEventArgs e)
+        {
+            GanttControl.DateMode = DateModes.Weekly;
+        }
+
+        private void ButtonMonthly_OnClick(object? sender, RoutedEventArgs e)
+        {
+            GanttControl.DateMode = DateModes.Monthly;
+        }
+
+        private void ButtonSeasonally_OnClick(object? sender, RoutedEventArgs e)
+        {
+            GanttControl.DateMode = DateModes.Seasonally;
+        }
+
+        private void ButtonYearly_OnClick(object? sender, RoutedEventArgs e)
+        {
+            GanttControl.DateMode = DateModes.Yearly;
+        }
+
+    }
+}
