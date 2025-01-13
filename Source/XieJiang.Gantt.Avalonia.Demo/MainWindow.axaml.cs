4fc69a7816d179fc665d16a5c194e2e2fc9295c7
xiejiang <xiejiang2014@icloud.com>
2025-01-14 02:24:12 +0800

    TreeDataGrid

--- Source/XieJiang.Gantt.Avalonia.Demo/MainWindow.axaml.cs
+++ Source/XieJiang.Gantt.Avalonia.Demo/MainWindow.axaml.cs
@@ -1,6 +1,9 @@
 using Avalonia.Controls;
+using Avalonia.Controls.Selection;
 using Avalonia.Interactivity;
 using System;
+using Avalonia.Controls.Models.TreeDataGrid;
+using Avalonia.Media;
 using XieJiang.Gantt.Avalonia.Models;
 
 namespace XieJiang.Gantt.Avalonia.Demo
@@ -14,6 +17,7 @@ namespace XieJiang.Gantt.Avalonia.Demo
             ganttModel.GanttTasks.Add(new GanttTask()
                                       {
                                           Id        = 1,
+                                          Title = "Open the refrigerator",
                                           Progress  = 0.5d,
                                           StartDate = new DateTime(2024, 12, 21),
                                           EndDate   = new DateTime(2024, 12, 23),
@@ -22,6 +26,7 @@ namespace XieJiang.Gantt.Avalonia.Demo
             ganttModel.GanttTasks.Add(new GanttTask()
                                       {
                                           Id        = 2,
+                                          Title = "Put the elephant in the refrigerator",
                                           Progress  = 0.2d,
                                           StartDate = new DateTime(2024, 12, 23),
                                           EndDate   = new DateTime(2024, 12, 26),
@@ -30,6 +35,7 @@ namespace XieJiang.Gantt.Avalonia.Demo
             ganttModel.GanttTasks.Add(new GanttTask()
                                       {
                                           Id        = 3,
+                                          Title = "Close the refrigerator",
                                           Progress  = 0.7d,
                                           StartDate = new DateTime(2024, 12, 27),
                                           EndDate   = new DateTime(2024, 12, 31),
@@ -43,11 +49,34 @@ namespace XieJiang.Gantt.Avalonia.Demo
                                       {
                                           DateTime = new DateTime(2025, 1, 6, 12, 30, 0),
                                           Title    = "Deadline hahaha",
-                                          Color = GanttColors.Success
+                                          Color    = GanttColors.Success
                                       });
 
-
             GanttControl.DataContext = ganttModel;
+
+            TreeDataGrid1.Source = new FlatTreeDataGridSource<GanttTask>(ganttModel.GanttTasks)
+                                   {
+                                       Columns =
+                                       {
+                                           //new TextColumn<GanttTask, string>("Country", x => x.Name, (r, v) => r.Name = v, new GridLength(6, GridUnitType.Star), new()
+                                           //                                      {
+                                           //                                          IsTextSearchEnabled = true,
+                                           //                                      }),
+                                           //new TemplateColumn<GanttTask>("Region", "RegionCell", "RegionEditCell"),
+                                           new TextColumn<GanttTask, int>("ID", x => x.Id, new GridLength(3,          GridUnitType.Auto)),
+                                           new TextColumn<GanttTask, string>("Title", x => x.Title, new GridLength(3, GridUnitType.Auto)),
+                                           new TextColumn<GanttTask, DateTime>("StartDate", x => x.StartDate, new GridLength(3, GridUnitType.Auto), new()
+                                                                                   {
+                                                                                       TextAlignment = TextAlignment.Right,
+                                                                                       MaxWidth      = new GridLength(150)
+                                                                                   }),
+                                           new TextColumn<GanttTask, DateTime>("EndDate", x => x.EndDate, new GridLength(3, GridUnitType.Auto), new()
+                                                                                   {
+                                                                                       TextAlignment = TextAlignment.Right,
+                                                                                       MaxWidth      = new GridLength(150)
+                                                                                   }),
+                                       }
+                                   };
         }
 
         protected override void OnLoaded(RoutedEventArgs e)
--- Source/XieJiang.Gantt.Avalonia/GanttTask.cs
+++ Source/XieJiang.Gantt.Avalonia/GanttTask.cs
@@ -88,6 +88,23 @@ public class GanttTask : INotifyPropertyChanged
 
     #endregion
 
+    #region Name
+
+    private string _title;
+
+    public string Title
+    {
+        get => _title;
+        set
+        {
+            if (value == _title) return;
+            _title = value;
+            OnPropertyChanged();
+        }
+    }
+
+    #endregion
+
 
     #region Id
 
