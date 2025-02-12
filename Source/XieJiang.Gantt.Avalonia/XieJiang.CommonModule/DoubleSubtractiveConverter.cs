0d7d4a142834e9d7277ec5cc704123b118e53ae7
xiejiang <xiejiang2014@icloud.com>
2025-02-13 05:38:27 +0800

    Missing files

---
+++ Source/XieJiang.Gantt.Avalonia/XieJiang.CommonModule/DoubleSubtractiveConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace XieJiang.CommonModule.Ava;
+
+public class DoubleSubtractiveConverter : IMultiValueConverter
+{
+    private static DoubleSubtractiveConverter? _default;
+    public static  DoubleSubtractiveConverter  Default => _default ??= new DoubleSubtractiveConverter();
+
+    public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (values?.Count != 2 || !targetType.IsAssignableFrom(typeof(double)))
+            throw new NotSupportedException();
+
+        if (values[0] is double d0 && values[1] is double d1)
+        {
+            return d0 - d1;
+        }
+
+
+        return null;
+    }
+}
